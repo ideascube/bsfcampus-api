fb26f3b208d2b46959fffa15ceb4f7a1b2019099
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-02-08 22:15:48 +0100

    MOOKBSF-138 #time 10m Added slugs to resource objects

--- MookAPI/resources/documents/__init__.py
+++ MookAPI/resources/documents/__init__.py
@@ -1,5 +1,6 @@
 from MookAPI import db
 import datetime
+from slugify import slugify
 
 class ResourceContent(db.DynamicEmbeddedDocument):
 	"""Generic collection, every resource type will inherit from this."""
@@ -23,6 +24,9 @@ class Resource(db.Document):
 
 	## Title of the resource
 	title = db.StringField(required=True)
+
+	## Slug
+	slug = db.StringField(unique=True)
 
 	## Creator should reference a user
 	## Will be implemented later
@@ -52,6 +56,22 @@ class Resource(db.Document):
 	resource_content = db.EmbeddedDocumentField(ResourceContent)
 
 	### METHODS
+
+	def set_slug(self):
+		slug = slugify(self.title) if self.slug is None else slugify(self.slug)
+		def alternate_slug(text, k=1):
+			return text if k <= 1 else "{text}-{k}".format(text=text, k=k)
+		k = 0
+		while k < 10**4:
+			if len(Resource.objects(slug=alternate_slug(slug, k))) > 0:
+				k = k + 1
+				continue
+			else:
+				break
+		self.slug = alternate_slug(slug, k)
+
+	def clean(self):
+		self.set_slug()
 
 	def __unicode__(self):
 		return self.title
