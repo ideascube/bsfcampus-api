d72a5686a0365f89f496b1562c0ba189261936d3
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-02-23 03:15:04 +0100

    Added route to GET one exercise attempt.

--- MookAPI/activity/views.py
+++ MookAPI/activity/views.py
@@ -10,10 +10,10 @@ from bson import json_util
 from flask_cors import cross_origin
 
 
-@bp.route("/ex_attempts", methods=['POST'])
+@bp.route("/exercise_attempts", methods=['POST'])
 @cross_origin() # allow all origins all methods.
 def post_exercise_attempt():
-	exercise_id = json.loads(flask.request.data)['attempt']['exercise']
+	exercise_id = flask.request.get_json()['exerciseAttempt']['exercise']
 	exercise = resources_documents.Resource.objects.get_or_404(id=exercise_id)
 
 	print ("CREATING exercise attempt for exercise {exercise}".format(exercise=exercise.id))
@@ -21,5 +21,13 @@ def post_exercise_attempt():
 	attempt = documents.exercise_attempt.ExerciseAttempt().init_with_exercise(exercise)
 	attempt.save()
 
-	return flask.jsonify(attempt=attempt)
+	return flask.jsonify(exercise_attempt=attempt)
 
+@bp.route("/exercise_attempts/<attempt_id>")
+def get_ex_attempt(attempt_id):
+	"""GET one exercise attempt"""
+
+	print ("GETTING exercise attempt with id {attempt_id}".format(attempt_id=attempt_id))
+	
+	ex_attempt = documents.exercise_attempt.ExerciseAttempt.objects.get_or_404(id=attempt_id)
+	return flask.jsonify(ex_attempt=ex_attempt)
