0e7072b643307857d2e05dba4c883ffd841e3e96
fredfourcade <frederic.fourcade@gmail.com>
2015-03-17 17:42:26 +0100

    removes old code

--- MookAPI/hierarchy/__init__.py
+++ MookAPI/hierarchy/__init__.py
@@ -1,24 +1,6 @@
 from flask import Blueprint
-from random import randint
 
 bp = Blueprint("hierarchy", __name__)
 
 import documents
 import views
-
-def getTrackValidated(trackId):
-	rd = randint(0, 99)
-	return (rd < 50)
-
-def getSkillValidated(skillId):
-	rd = randint(0, 99)
-	return (rd < 50)
-
-def progress(trackId):
-	track = documents.Track.get_unique_object_or_404(track_id)
-	nbValidatedSkill = 0
-	skillList = track.skills()
-	for skill in skillList:
-		skillValidated = getSkillValidated(skill.id)
-		if skillValidated: nbValidatedSkill += 1
-	return nbValidatedSkill/len(skillList) if len(skillList) > 0 else 100
