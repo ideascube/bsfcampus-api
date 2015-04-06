1c8102365d6c64b8fa3e526529cf726b83a79dd7
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-04-06 12:28:56 +0200

    Allows the server to be accessed with its LAN IP when using GUnicorn

--- gunicorn-config.py
+++ gunicorn-config.py
@@ -1,5 +1,11 @@
+## This is a trick to find out the server's LAN IP address
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 0))
+local_ip_address = s.getsockname()[0]
+
 ## Adress where the server can be accessed
-bind = ['127.0.0.1:5000']
+bind = ['127.0.0.1:5000', local_ip_address + ':5000']
 
 ## Restart the server when the code changes
 reload = True
