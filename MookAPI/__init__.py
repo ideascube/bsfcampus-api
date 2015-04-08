2736fea31dc3ea87a86e52f5e062bdbf11275dd3
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-04-08 17:57:02 +0200

    Adds a config file to determine the server is central or local and decorators to define functions/classes only for a given type of server.

--- MookAPI/__init__.py
+++ MookAPI/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.mongoengine import MongoEngine
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.mongoengine import ModelView
 from flask_cors import CORS
-
+import app_config
 
 ### CREATE FLASK APP
 app = Flask(__name__)
@@ -29,6 +29,26 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
+### CENTRAL-SERVER-ONLY AND LOCAL-SERVER-ONLY DECORATORS
+## Central-server-only
+def if_central(func):
+	if app_config.server_type == 'central':
+		return func
+	else:
+		def empty_function(*args, **kwargs):
+			pass
+		return empty_function
+
+## Local-server-only
+def if_local(func):
+	if app_config.server_type == 'local':
+		return func
+	else:
+		def empty_function(*args, **kwargs):
+			pass
+		return empty_function
+
+
 ### LOAD APP-LEVEL MODULES
 ## Views: define HTTP endpoints
 import views
@@ -49,30 +69,34 @@ import config
 app.register_blueprint(config.bp, url_prefix="/config")
 
 ### ADMINISTRATION INTERFACE
-admin = Admin(app)
-## Exercise resources
-import resources.documents.exercise
-admin.add_view(ModelView(resources.documents.exercise.ExerciseResource))
-## Rich text resources
-import resources.documents.rich_text
-admin.add_view(ModelView(resources.documents.rich_text.RichTextResource))
-## External video resources
-import resources.documents.external_video
-admin.add_view(ModelView(resources.documents.external_video.ExternalVideoResource))
-## Audio resources
-import resources.documents.audio
-admin.add_view(ModelView(resources.documents.audio.AudioResource))
-## Video resources
-import resources.documents.video
-admin.add_view(ModelView(resources.documents.video.VideoResource))
-## Downloadable file resources
-import resources.documents.downloadable_file
-admin.add_view(ModelView(resources.documents.downloadable_file.DownloadableFileResource))
-## Tracks
-import hierarchy.documents as hierarchy_documents
-admin.add_view(ModelView(hierarchy_documents.Track))
-admin.add_view(ModelView(hierarchy_documents.Skill))
-admin.add_view(ModelView(hierarchy_documents.Lesson))
-## Config
-import config.documents
-admin.add_view(ModelView(config.documents.ConfigParameters))
+@if_central
+def create_admin_interface():
+	admin = Admin(app)
+	## Exercise resources
+	import resources.documents.exercise
+	admin.add_view(ModelView(resources.documents.exercise.ExerciseResource))
+	## Rich text resources
+	import resources.documents.rich_text
+	admin.add_view(ModelView(resources.documents.rich_text.RichTextResource))
+	## External video resources
+	import resources.documents.external_video
+	admin.add_view(ModelView(resources.documents.external_video.ExternalVideoResource))
+	## Audio resources
+	import resources.documents.audio
+	admin.add_view(ModelView(resources.documents.audio.AudioResource))
+	## Video resources
+	import resources.documents.video
+	admin.add_view(ModelView(resources.documents.video.VideoResource))
+	## Downloadable file resources
+	import resources.documents.downloadable_file
+	admin.add_view(ModelView(resources.documents.downloadable_file.DownloadableFileResource))
+	## Tracks
+	import hierarchy.documents as hierarchy_documents
+	admin.add_view(ModelView(hierarchy_documents.Track))
+	admin.add_view(ModelView(hierarchy_documents.Skill))
+	admin.add_view(ModelView(hierarchy_documents.Lesson))
+	## Config
+	import config.documents
+	admin.add_view(ModelView(config.documents.ConfigParameters))
+
+create_admin_interface()
