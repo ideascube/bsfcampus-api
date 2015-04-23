eb45f776665fc2ea0e33c0ecb5d11baf462ec63e
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-04-23 13:20:08 +0200

    Bugfix in GUnicorn config

--- gunicorn-config.py
+++ gunicorn-config.py
@@ -1,7 +1,7 @@
 ## This is a trick to find out the server's LAN IP address
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('8.8.8.8', 0))
+s.connect(('8.8.8.8', 80))
 local_ip_address = s.getsockname()[0]
 
 ## Adress where the server can be accessed
@@ -9,3 +9,7 @@ bind = ['127.0.0.1:5000', local_ip_address + ':5000']
 
 ## Restart the server when the code changes
 reload = True
+
+## Number of workers (allows concurrent requests)
+import multiprocessing
+workers = multiprocessing.cpu_count() * 2 + 1
