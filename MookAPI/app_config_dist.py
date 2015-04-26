c065f915270bfcafd965e0424476f09d665f842c
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-04-26 17:33:24 +0200

    Added central_server_api_host parameter, and added explanations on app configuration file.

--- MookAPI/app_config_dist.py
+++ MookAPI/app_config_dist.py
@@ -1,5 +1,11 @@
 ### Make a copy of this file called 'app_config.py'
 ### and change the values of the parameters to what you actually want.
 
-server_type = 'central'
+## This salt is used to encrpyt data. Use http://randomkeygen.com/ to generate a random string.
 password_salt = 'Find a nice salt to put here'
+
+## Is this a central or a local server? Choose 'central' or 'local'.
+server_type = 'central'
+
+## If this is a local server, give the address of the associated central server
+central_server_api_host = 'http://HOSTNAME:PORT/'
