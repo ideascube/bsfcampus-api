af121912555f5bae0979630976811b51f483f0fa
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-05-01 15:25:59 +0200

    Provides more helpful information on errors in creating local copy of document.

--- MookAPI/synchronizer/views.py
+++ MookAPI/synchronizer/views.py
@@ -91,7 +91,7 @@ def create_object(document_class, url):
 			bson_object = bson.json_util.loads(r.text)
 			obj = document_class.init_with_json_result(bson_object)
 		except:
-			return None, str(sys.exc_info()[0])
+			return None, str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1])
 
 		return obj, None
 
