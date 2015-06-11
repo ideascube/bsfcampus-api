d075f2ca7d83c5111b3b94edd2ae17ce942d10b8
Frédéric Fourcade <frederic.fourcade@gryzz-lab.fr>
2015-06-11 17:20:16 +0200

    [#MOOKBSF-368] Adds a POST route for the /users/current route to modify logged in User profile

--- MookAPI/users/views.py
+++ MookAPI/users/views.py
@@ -4,30 +4,50 @@ import json
 from . import bp
 from MookAPI import api
 from flask.ext.security import login_required
-from flask.ext import restful
+from flask_restful import Resource, reqparse
 import flask.ext.security as security
-from flask.ext.security.core import AnonymousUser
-from flask_cors import cross_origin
+from flask_mongoengine import ValidationError
 
-class CurrentUserView(restful.Resource):
+class CurrentUserView(Resource):
 
     def get(self):
         """Get the current logged in user data"""
         print ("get current user")
 
-        try:
-            current_user_id = security.current_user.id
-        except:
+        if not security.current_user.id:
             response = {"status_code": 401}
             return response
-        else:
-            print (current_user_id)
+
+        current_user_id = security.current_user.id
+        print (current_user_id)
+        return documents.User.get_unique_object_or_404(current_user_id)
+
+    @login_required
+    def post(self):
+        print ("current user post")
+        parser = reqparse.RequestParser()
+        parser.add_argument('full_name', required=True, type=unicode)
+        parser.add_argument('email', required=True, type=unicode)
+        args = parser.parse_args()
+        if security.current_user.id:
+            security.current_user.full_name = args.full_name
+            security.current_user.email = args.email
+            try:
+                security.current_user.save()
+            except ValidationError as e:
+                response = {"status_code": 400, "message": e.message}
+                return response
+
+            current_user_id = security.current_user.id
             return documents.User.get_unique_object_or_404(current_user_id)
+        else:
+            response = {"status_code": 404}
+            return response
 
 api.add_resource(CurrentUserView, '/users/current', endpoint='current_user')
 
 
-class UserView(restful.Resource):
+class UserView(Resource):
 
     def get(self, user_id):
         """Get the user data defined by the given user_id"""
@@ -37,7 +57,7 @@ class UserView(restful.Resource):
 api.add_resource(UserView, '/users/<user_id>', endpoint='user')
 
 
-class LogoutCurrentUserView(restful.Resource):
+class LogoutCurrentUserView(Resource):
 
     @login_required
     def post(self):
