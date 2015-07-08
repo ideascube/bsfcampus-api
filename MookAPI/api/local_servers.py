eeac23d3e967dc7c0ee367d3f3ae13b111cdc048
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-07-09 01:06:04 +0200

    Replaces local server views protection with basic auth

--- MookAPI/api/local_servers.py
+++ MookAPI/api/local_servers.py
@@ -1,19 +1,18 @@
 import datetime
 
 from flask import Blueprint, jsonify
-from flask_jwt import current_user
-from MookAPI.auth import jwt_required
 
 from MookAPI.services import local_servers
 
+from ._security import basic_auth_required, basic_auth_user
 from . import route
 
 bp = Blueprint("local_servers", __name__, url_prefix="/local_servers")
 
 @route(bp, "/reset")
-@jwt_required()
+@basic_auth_required
 def reset_local_server():
-    user = current_user._get_current_object()
+    user = basic_auth_user._get_current_object()
     # FIXME: Check if user has role "local_server"
 
     local_server = local_servers.first(user=user)
@@ -26,9 +25,9 @@ def reset_local_server():
     return jsonify(data=local_server)
 
 @route(bp, "/sync")
-@jwt_required()
+@basic_auth_required
 def get_local_server_sync_list():
-    user = current_user._get_current_object()
+    user = basic_auth_user._get_current_object()
     #FIXME: Check if user has role "local_server"
 
     local_server = local_servers.first(user=user)
@@ -42,13 +41,13 @@ def get_local_server_sync_list():
 
     local_server.save()
 
-    return jsonify(items)
+    return jsonify(items=items)
 
-@route(bp, "/register")
-@jwt_required()
+@route(bp, "/register", methods=['POST'])
+@basic_auth_required
 def register_local_server():
 
-    user = current_user._get_current_object()
+    user = basic_auth_user._get_current_object()
     #FIXME: Check if user has role "local_server"
 
     local_server = local_servers.create(user=user)
