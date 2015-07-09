f2484efff4452e575c60b5774696262a454058f9
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-07-09 21:48:27 +0200

    Sets default SERVER_TYPE to central.

--- MookAPI/utils.py
+++ MookAPI/utils.py
@@ -1,7 +1,8 @@
 from flask import current_app
 
 def is_local():
-    return current_app.config['SERVER_TYPE'] == 'local'
+    server_type = current_app.config.get('SERVER_TYPE', 'central')
+    return server_type == 'local'
 
 def is_central():
     return not is_local()
