774e14bcf20ebe8817ea795af25b7a03fe94c7ce
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-07-19 17:26:09 +0200

    Bug fix in video resource serialization

--- MookAPI/resources/documents/video.py
+++ MookAPI/resources/documents/video.py
@@ -9,8 +9,11 @@ from .linked_file import LinkedFileResourceContentJsonSerializer, \
 class VideoResourceContentJsonSerializer(LinkedFileResourceContentJsonSerializer):
 
     # FIXME we should to this using the __json_*__ properties instead.
-    def encode_mongo(self, fields=None):
-        rv = super(VideoResourceContentJsonSerializer, self).encode_mongo(fields)
+    def encode_mongo(self, fields=None, for_distant=False):
+        rv = super(VideoResourceContentJsonSerializer, self).encode_mongo(
+            fields=fields,
+            for_distant=for_distant
+        )
 
         if is_local():
             del rv['source']
