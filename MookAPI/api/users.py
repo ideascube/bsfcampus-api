4a29e7fcdbb4be1bc08955fdadfa362b067856d9
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-07-22 03:45:52 +0200

    Fixes a bug where the central server would try to add new users to their LocalServer instance's sync list, but they don't have one.

--- MookAPI/api/users.py
+++ MookAPI/api/users.py
@@ -197,8 +197,10 @@ def register_user():
         )
         ## Password is automatically hashed in the Service.
 
-        local_server.append_syncable_item(document=new_user)
-        local_server.save()
+        if local_server:
+            local_server.append_syncable_item(document=new_user)
+            local_server.clean()
+            local_server.save(validate=False) # FIXME Do validation when MongoEngine bug is fixed.
 
         activity.record_misc_analytic("register_user_attempt", "success")
         return jsonify(data=new_user)
