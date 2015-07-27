349d969c4700784c5f3e98c93c87bbf4b07b9e0e
Frédéric Fourcade <frederic.fourcade@gryzz-lab.fr>
2015-07-27 19:29:08 +0200

    Changes the blueprint name and route for all static pages

--- MookAPI/api/static_pages.py
+++ MookAPI/api/static_pages.py
@@ -4,13 +4,13 @@ from MookAPI.services import static_pages
 
 from . import route
 
-bp = Blueprint('static_pages', __name__, url_prefix="/static_pages")
+bp = Blueprint('static_page', __name__, url_prefix="/static_page")
 
 
 @route(bp, "/<page_id>")
 def get_static_page(page_id):
     return static_pages.get(page_id=page_id)
 
-@route(bp, "/")
+@route(bp, "")
 def all_static_pages():
     return static_pages.all()
