d03070a531b46ce18b9732c0972042b9e97c97d5
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-09-17 16:44:03 +0200

    Adds API route to stream static files

---
+++ MookAPI/api/file.py
@@ -0,0 +1,40 @@
+import os, re, mimetypes
+
+from flask import Blueprint, request, send_file, Response, current_app
+
+from . import route
+
+bp = Blueprint("file", __name__, url_prefix="/file")
+
+
+@route(bp, "/<filename>")
+def get_static_file(filename):
+    path = os.path.join(current_app.static_folder, filename)
+    range_header = request.headers.get('Range', None)
+    if not range_header: return send_file(path)
+
+    size = os.path.getsize(path)
+    byte1, byte2 = 0, None
+
+    m = re.search('(\d+)-(\d*)', range_header)
+    g = m.groups()
+
+    if g[0]: byte1 = int(g[0])
+    if g[1]: byte2 = int(g[1])
+
+    length = size - byte1
+    if byte2 is not None:
+        length = byte2 - byte1
+
+    data = None
+    with open(path, 'rb') as f:
+        f.seek(byte1)
+        data = f.read(length)
+
+    rv = Response(data,
+                  206,
+                  mimetype=mimetypes.guess_type(path)[0],
+                  direct_passthrough=True)
+    rv.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
+
+    return rv
