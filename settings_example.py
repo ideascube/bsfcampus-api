96209243887ecaae35a4a3338933edced9227869
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-09-25 14:49:07 +0200

    Updates settings file to handle static files

--- settings_example.py
+++ settings_example.py
@@ -4,6 +4,8 @@ class Config(object):
     SECRET_KEY = 'Find a nice secret key to protect the app.' # Use e.g. http://randomkeygen.com/
     MONGODB_DB = 'bsfcampus'
     CORS_ORIGINS = ['http://localhost', 'http://localhost:63342']
+    UPLOAD_FILES_PATH = "/path/to/static/"
+    UPLOAD_FILES_URL = "http://url_to_static/"
     ## Local servers only:
     CENTRAL_SERVER_HOST = 'http://localhost:5000'
     CENTRAL_SERVER_KEY = ''
