6b249923af70cb29378d6ece2bef2a9bf653694f
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-11-04 22:00:00 +0100

    Reorganizes the back office, in particular, moves the analytics view

--- MookAPI/admin/__init__.py
+++ MookAPI/admin/__init__.py
@@ -1,85 +1,8 @@
-from .views import ProtectedModelView
+from .views import *
 from flask.ext.admin import Admin
-from wtforms import ValidationError
-from flask.ext.admin.contrib.mongoengine import ModelView
+from MookAPI.services import *
 
-from MookAPI.services import \
-    users, \
-    tracks, \
-    skills, \
-    lessons, \
-    exercise_resources, \
-    track_validation_resources, \
-    rich_text_resources, \
-    audio_resources, \
-    video_resources, \
-    downloadable_file_resources, \
-    roles, \
-    local_servers, \
-    static_pages
-
-# Validators
-def parent_resource_required_if_additional(form, field):
-    if form.data['is_published'] and form.data['is_additional'] and field.data is None:
-        raise ValidationError('An additional resource must have a parent resource')
-
-def no_parent_additional_resource_if_additional(form, field):
-    if form.data['is_additional'] and field.data is not None and field.data['is_additional']:
-        raise ValidationError('An additional resource must not have another additional resource as parent')
-
-def no_parent_resource_if_not_additional(form, field):
-    if not form.data['is_additional'] and field.data is not None:
-        raise ValidationError('An standard resource must not have a parent resource')
-
-def parent_if_not_additional(form, field):
-    if form.data['is_published'] and not form.data['is_additional'] and field.data is None:
-        raise ValidationError('A published standard resource must have a parent')
-
-def no_parent_if_additional(form, field):
-    if form.data['is_additional'] and field.data is not None:
-        raise ValidationError('An additional resource must not have a parent')
-
-
-# Admin views
-class UserView(ModelView):
-    column_list = ('full_name', 'email', 'accept_cgu', 'roles')
-    form_columns = ('full_name', 'email', 'accept_cgu', 'roles')
-
-class ResourceView(ProtectedModelView):
-    column_list = ('is_additional', 'is_published', 'title', 'slug', 'description', 'order', 'keywords', 'parent', 'parent_resource')
-    form_columns = ('is_additional', 'is_published', 'title', 'slug', 'description', 'order', 'keywords', 'parent', 'parent_resource', 'resource_content')
-
-    form_args = dict(
-        parent_resource=dict(validators=[parent_resource_required_if_additional, no_parent_resource_if_not_additional, no_parent_additional_resource_if_additional]),
-        parent=dict(validators=[parent_if_not_additional, no_parent_if_additional])
-    )
-
-
-class HierarchyTrackView(ProtectedModelView):
-    column_list = ('is_active', 'is_published', 'title', 'slug', 'description', 'order', 'icon')
-    form_columns = ('is_active', 'is_published', 'title', 'slug', 'description', 'order', 'icon')
-
-
-class HierarchySkillView(ProtectedModelView):
-    column_list = ('is_published', 'title', 'slug', 'description', 'short_description', 'track', 'order', 'icon')
-    form_columns = ('is_published', 'title', 'slug', 'description', 'short_description', 'track', 'order', 'icon', 'validation_exercise')
-
-
-class HierarchyLessonView(ProtectedModelView):
-    column_list = ('is_published', 'title', 'slug', 'description', 'order', 'skill')
-    form_columns = ('is_published', 'title', 'slug', 'description', 'order', 'skill')
-
-class LocalServerView(ProtectedModelView):
-    def on_model_change(self, form, model, is_created):
-        if not model.secret.startswith("$2a$"):
-            model.secret = self.model.hash_secret(model.secret)
-        return
-
-class StaticPageView(ProtectedModelView):
-    pass
-
-
-admin = Admin()
+admin = Admin(template_mode="bootstrap3")
 
 admin.add_view(StaticPageView(
     static_pages.__model__,
@@ -101,11 +24,6 @@ admin.add_view(ResourceView(
     rich_text_resources.__model__,
     name='Rich Text',
     category='Resources'))
-
-# admin.add_view(ResourceView(
-#     external_video_resources.__model__,
-#     name='External Video',
-#     category='Resources'))
 
 admin.add_view(ResourceView(
     audio_resources.__model__,
@@ -156,3 +74,5 @@ admin.add_view(LocalServerView(
     local_servers.__model__,
     name='Local server',
     category='Authentication'))
+
+admin.add_view(AnalyticsView(name="Analytics", endpoint="admin_analytics"))
--- MookAPI/admin/views.py
+++ MookAPI/admin/views.py
@@ -1,8 +1,11 @@
+from wtforms import ValidationError
+
 from flask import abort, redirect, url_for, request
 
 from flask.ext.admin.contrib.mongoengine import ModelView
-# from flask_security import current_user
 from flask.ext.admin.contrib.fileadmin import FileAdmin
+from flask.ext.admin import BaseView, expose
+# from flask_security import current_user
 
 
 class ProtectedAdminViewMixin:
@@ -14,12 +17,12 @@ class ProtectedAdminViewMixin:
         #     return True
         # return False
 
-    # def _handle_view(self, name, **kwargs):
-    #     if not self.is_accessible():
-    #         if not current_user.is_anonymous:
-    #             abort(403)
-    #         else:
-    #             return redirect(url_for('security.login', next=request.url))
+        # def _handle_view(self, name, **kwargs):
+        #     if not self.is_accessible():
+        #         if not current_user.is_anonymous:
+        #             abort(403)
+        #         else:
+        #             return redirect(url_for('security.login', next=request.url))
 
 
 class ProtectedFileAdmin(ProtectedAdminViewMixin, FileAdmin):
@@ -28,3 +31,80 @@ class ProtectedFileAdmin(ProtectedAdminViewMixin, FileAdmin):
 
 class ProtectedModelView(ProtectedAdminViewMixin, ModelView):
     pass
+
+
+def parent_resource_required_if_additional(form, field):
+    if form.data['is_published'] and form.data['is_additional'] and field.data is None:
+        raise ValidationError('An additional resource must have a parent resource')
+
+
+def no_parent_additional_resource_if_additional(form, field):
+    if form.data['is_additional'] and field.data is not None and field.data['is_additional']:
+        raise ValidationError('An additional resource must not have another additional resource as parent')
+
+
+def no_parent_resource_if_not_additional(form, field):
+    if not form.data['is_additional'] and field.data is not None:
+        raise ValidationError('An standard resource must not have a parent resource')
+
+
+def parent_if_not_additional(form, field):
+    if form.data['is_published'] and not form.data['is_additional'] and field.data is None:
+        raise ValidationError('A published standard resource must have a parent')
+
+
+def no_parent_if_additional(form, field):
+    if form.data['is_additional'] and field.data is not None:
+        raise ValidationError('An additional resource must not have a parent')
+
+
+class UserView(ProtectedModelView):
+    column_list = ('full_name', 'email', 'accept_cgu', 'roles')
+    form_columns = ('full_name', 'email', 'accept_cgu', 'roles')
+
+
+class ResourceView(ProtectedModelView):
+    column_list = (
+    'is_additional', 'is_published', 'title', 'slug', 'description', 'order', 'keywords', 'parent', 'parent_resource')
+    form_columns = (
+    'is_additional', 'is_published', 'title', 'slug', 'description', 'order', 'keywords', 'parent', 'parent_resource',
+    'resource_content')
+
+    form_args = dict(
+        parent_resource=dict(validators=[parent_resource_required_if_additional, no_parent_resource_if_not_additional,
+                                         no_parent_additional_resource_if_additional]),
+        parent=dict(validators=[parent_if_not_additional, no_parent_if_additional])
+    )
+
+
+class HierarchyTrackView(ProtectedModelView):
+    column_list = ('is_active', 'is_published', 'title', 'slug', 'description', 'order', 'icon')
+    form_columns = ('is_active', 'is_published', 'title', 'slug', 'description', 'order', 'icon')
+
+
+class HierarchySkillView(ProtectedModelView):
+    column_list = ('is_published', 'title', 'slug', 'description', 'short_description', 'track', 'order', 'icon')
+    form_columns = ('is_published', 'title', 'slug', 'description', 'short_description', 'track', 'order', 'icon',
+                    'validation_exercise')
+
+
+class HierarchyLessonView(ProtectedModelView):
+    column_list = ('is_published', 'title', 'slug', 'description', 'order', 'skill')
+    form_columns = ('is_published', 'title', 'slug', 'description', 'order', 'skill')
+
+
+class LocalServerView(ProtectedModelView):
+    def on_model_change(self, form, model, is_created):
+        if not model.secret.startswith("$2a$"):
+            model.secret = self.model.hash_secret(model.secret)
+        return
+
+
+class StaticPageView(ProtectedModelView):
+    pass
+
+
+class AnalyticsView(ProtectedAdminViewMixin, BaseView):
+    @expose("/")
+    def index(self):
+        return self.render("admin/analytics.html")
